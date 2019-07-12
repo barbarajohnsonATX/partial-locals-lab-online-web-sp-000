e4d284e021e303253e28135c11b44ba20ce29f79
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-12 03:53:05 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,12 +14,14 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
   
-  def self.search(query)
-    if query.present?
-      where('NAME like ?', "%#{query}%")
-    else
-      self.all
+   def self.search(search)
+      if search.blank?
+        self.all
+      else
+        result = self.all.find_all do |student|
+          student.name.downcase.include?(search.downcase)
+        end.compact
+      end
     end
-  end
   
 end
